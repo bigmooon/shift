3f523c900b338a7a9775a97f8a1a6ec45694f6cf
bigmooon <bigmooon1215@gmail.com>
2025-09-28 18:52:36 +0900

    style: improve landing page and start host page styles

--- react/src/pages/Landing.js
+++ react/src/pages/Landing.js
@@ -1,77 +1,71 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/scss";
-import "swiper/scss/effect-cards";
-import "swiper/scss/autoplay";
-import "../assets/styles/Landing.scss";
-import { EffectCards, Autoplay } from "swiper/modules";
-import { Button } from "../components/Button/Button";
-import TRO from "../assets/images/TRO.png";
-import TRS from "../assets/images/TRS.png";
-import TCO from "../assets/images/TCO.png";
-import TCS from "../assets/images/TCS.png";
-import PRO from "../assets/images/PRO.png";
-import PRS from "../assets/images/PRS.png";
-import PCO from "../assets/images/PCO.png";
-import PCS from "../assets/images/PCS.png";
-import { saveDataWithExpiration } from "../components/CookieUtils/SecureLocalStorageExtends";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/scss';
+import 'swiper/scss/effect-cards';
+import 'swiper/scss/autoplay';
+import '../assets/styles/Landing.scss';
+import { EffectCards, Autoplay } from 'swiper/modules';
+import { Button } from '../components/Button/Button';
+import TRO from '../assets/images/TRO.png';
+import TRS from '../assets/images/TRS.png';
+import TCO from '../assets/images/TCO.png';
+import TCS from '../assets/images/TCS.png';
+import PRO from '../assets/images/PRO.png';
+import PRS from '../assets/images/PRS.png';
+import PCO from '../assets/images/PCO.png';
+import PCS from '../assets/images/PCS.png';
+import { saveDataWithExpiration } from '../components/CookieUtils/SecureLocalStorageExtends';
 
 const landingImgs = [
-  { src: TRO, alt: "TRO", style: { objectFit: "cover" } },
-  { src: TRS, alt: "TRS" },
-  { src: TCO, alt: "TCO" },
-  { src: TCS, alt: "TCS" },
-  { src: PRO, alt: "PRO", style: { objectFit: "cover" } },
-  { src: PRS, alt: "PRS" },
-  { src: PCO, alt: "PCO", style: { objectFit: "cover" } },
-  { src: PCS, alt: "PCS", style: { objectFit: "cover" } },
+  { src: TRO, alt: 'TRO', style: { objectFit: 'cover' } },
+  { src: TRS, alt: 'TRS' },
+  { src: TCO, alt: 'TCO' },
+  { src: TCS, alt: 'TCS' },
+  { src: PRO, alt: 'PRO', style: { objectFit: 'cover' } },
+  { src: PRS, alt: 'PRS' },
+  { src: PCO, alt: 'PCO', style: { objectFit: 'cover' } },
+  { src: PCS, alt: 'PCS', style: { objectFit: 'cover' } },
 ];
 
 const Landing = () => {
   const navigate = useNavigate();
 
   const setShowLanding = () => {
-    navigate("/");
-    saveDataWithExpiration("hasVisitedLanding", true);
+    saveDataWithExpiration('hasVisitedLanding', true, 60 * 24); // maintain for 24 hours
+    navigate('/', { state: { from: '/landing' } });
   };
-  
+
   return (
     <div id="Container" className="landingContainer">
       <div className="landingWrapper">
         <div className="landingTitle">
           <span>
             남이 보는 나는,
-            <br/>
+            <br />
             과연 <span className="purpleText">어떤 사람</span>일까?
           </span>
         </div>
         <div className="imgBox">
           <Swiper
-            effect={"cards"}
-            modules={[EffectCards, Autoplay ]}
+            effect={'cards'}
+            modules={[EffectCards, Autoplay]}
             autoplay={{ delay: 2000, disableOnInteraction: false }}
             className="mySwiper"
           >
             {landingImgs.map((img, idx) => (
               <SwiperSlide key={idx}>
-                <img src={img.src} alt={img.alt} style={img.style}/>
+                <img src={img.src} alt={img.alt} style={img.style} />
               </SwiperSlide>
             ))}
           </Swiper>
         </div>
-        <Button 
-          color="#A778C2"
-          width={22.27}
-          height={3.328}
-          className="startBtn"
-          onClick={setShowLanding}
-        >
+        <Button color="#A778C2" className="startBtn" onClick={setShowLanding}>
           <span>시작하기</span>
         </Button>
       </div>
     </div>
-  )
+  );
 };
 
 export default Landing;
--- react/src/pages/LinkSender/StartHost.js
+++ react/src/pages/LinkSender/StartHost.js
@@ -1,23 +1,30 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import "../../assets/styles/LinkSender/StartHost.scss";
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../../assets/styles/LinkSender/StartHost.scss';
 import { Button } from '../../components/Button/Button.js';
 import { MainFooter } from '../../components/Footer/MainFooter.js';
-import shImg1 from "../../assets/images/StartHost/shImg1.png";
-import shImg2 from "../../assets/images/StartHost/shImg2.png";
-import shImg3 from "../../assets/images/StartHost/shImg3.png";
-import shImg4 from "../../assets/images/StartHost/shImg4.png";
-import shareImg from "../../assets/images/StartHost/shareImg.svg";
-import TryFetch from "../../components/FetchComponent/FetchComponent.js";
-import HandleLogin from "../../components/Login/HandleLogin.js";
-import { saveDataWithExpiration, loadDataWithExpiration, loadUserData } from "../../components/CookieUtils/SecureLocalStorageExtends.js";
-import { ShareTestUrl, ShareNavigator } from "../../components/Share/ShareComponent.js";
-
+import shImg1 from '../../assets/images/StartHost/shImg1.png';
+import shImg2 from '../../assets/images/StartHost/shImg2.png';
+import shImg3 from '../../assets/images/StartHost/shImg3.png';
+import shImg4 from '../../assets/images/StartHost/shImg4.png';
+import shareImg from '../../assets/images/StartHost/shareImg.svg';
+import TryFetch from '../../components/FetchComponent/FetchComponent.js';
+import HandleLogin from '../../components/Login/HandleLogin.js';
+import {
+  saveDataWithExpiration,
+  loadDataWithExpiration,
+  loadUserData,
+} from '../../components/CookieUtils/SecureLocalStorageExtends.js';
+import {
+  ShareTestUrl,
+  ShareNavigator,
+} from '../../components/Share/ShareComponent.js';
 
 const StartHost = () => {
-  const [ totalNum, setTotalNum ] = useState(0);
-  const [ test, setTest ] = useState(loadUserData("myTests")); // [tid, number]
-  const [ canSeeResult, setCanSeeResult ] = useState(test ? true : false);
+  const [totalNum, setTotalNum] = useState(0);
+  const [test, setTest] = useState(loadUserData('myTests')); // [tid, number]
+  const [canSeeResult, setCanSeeResult] = useState(test ? true : false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const [gradientValue, setGradientValue] = useState(0);
   const [increase, setIncrease] = useState(true);
@@ -25,26 +32,54 @@ const StartHost = () => {
   const navigate = useNavigate();
 
   const handleStart = () => {
-    HandleLogin({
-      assertLogin: true,
-      navigate: navigate,
-      toWhere: "/host/info",
-      onLoginSuccess: () => {
-        TryFetch("my_tests", "GET", {}, (data) => {
+    setIsLoading(true);
+
+    const isLogin = loadUserData('isLogin');
+
+    if (isLogin) {
+      TryFetch(
+        'my_tests',
+        'GET',
+        {},
+        (data) => {
           setTest(data);
+          setIsLoading(false);
           if (data.epa) {
-            alert("이미 진행중인 테스트가 있습니다. 진행을 완료하시면 기존 테스트를 삭제합니다.");
+            alert(
+              '이미 진행중인 테스트가 있습니다. 진행을 완료하시면 기존 테스트를 삭제합니다.'
+            );
           }
-        });
-      }
-    });
+          navigate('/host/info');
+        },
+        () => {
+          setIsLoading(false);
+        }
+      );
+    } else {
+      setIsLoading(false);
+      HandleLogin({
+        assertLogin: true,
+        navigate: navigate,
+        toWhere: '/host/info',
+        onLoginSuccess: () => {
+          TryFetch('my_tests', 'GET', {}, (data) => {
+            setTest(data);
+            if (data.epa) {
+              alert(
+                '이미 진행중인 테스트가 있습니다. 진행을 완료하시면 기존 테스트를 삭제합니다.'
+              );
+            }
+          });
+        },
+      });
+    }
   };
 
   const handleResult = () => {
     HandleLogin({
       assertLogin: true,
       navigate: navigate,
-      toWhere: "/result/dashboard"
+      toWhere: '/result/dashboard',
     });
   };
 
@@ -52,25 +87,25 @@ const StartHost = () => {
     HandleLogin({
       assertLogin: true,
       onLoginSuccess: () => {
-        TryFetch("my_tests", "GET", {}, (data) => {
+        TryFetch('my_tests', 'GET', {}, (data) => {
           setTest(data);
           if (data.epa === null || data.epa === undefined) {
-            alert("진행중인 테스트가 없습니다.");
+            alert('진행중인 테스트가 없습니다.');
           } else {
-            ShareTestUrl({tid:data.epa[0], nickname: data.epa[2]});
+            ShareTestUrl({ tid: data.epa[0], nickname: data.epa[2] });
           }
         });
-      }
+      },
     });
   };
 
   useEffect(() => {
-    const before = loadDataWithExpiration("totalNum");
+    const before = loadDataWithExpiration('totalNum');
     if (before !== null) {
       setTotalNum(before);
     } else {
-      TryFetch("total_num", "GET", {}, (data) => {
-        saveDataWithExpiration("totalNum", data.total_num, 1);
+      TryFetch('total_num', 'GET', {}, (data) => {
+        saveDataWithExpiration('totalNum', data.total_num, 1);
         setTotalNum(data.total_num);
       });
     }
@@ -80,18 +115,18 @@ const StartHost = () => {
       return;
     }
 
-    let t = loadUserData("myTests");
+    let t = loadUserData('myTests');
     if (t) {
       setTest(t);
       return;
     }
-    TryFetch("my_tests", "GET", {}, (data) => {
+    TryFetch('my_tests', 'GET', {}, (data) => {
       if (data.epa !== null && data.epa !== undefined) {
         setTest(data);
         setCanSeeResult(true);
       }
     });
-  }, []);
+  }, [test]);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -110,7 +145,6 @@ const StartHost = () => {
     return () => clearInterval(interval);
   }, [increase, gradientValue]);
 
-
   return (
     <>
       <div id="Container" className="shContainer">
@@ -125,7 +159,7 @@ const StartHost = () => {
                 <span>수치로 확인하는 자기 객관성</span>
               </div>
               <div className="shBoxImg">
-                <img src={shImg1} alt="boxImg"/>
+                <img src={shImg1} alt="boxImg" />
               </div>
               <div className="shBoxMainTitle">
                 <span>나는 상위 몇 % 일까?</span>
@@ -136,7 +170,7 @@ const StartHost = () => {
                 <span>나를 어떻게 생각할까?</span>
               </div>
               <div className="shBoxImg">
-                <img src={shImg2} alt="boxImg"/>
+                <img src={shImg2} alt="boxImg" />
               </div>
               <div className="shBoxMainTitle">
                 <span>친구에게 공유하기</span>
@@ -147,7 +181,7 @@ const StartHost = () => {
                 <span>내가 아는 나 vs 남이 아는 나</span>
               </div>
               <div className="shBoxImg">
-                <img src={shImg3} alt="boxImg"/>
+                <img src={shImg3} alt="boxImg" />
               </div>
               <div className="shBoxMainTitle">
                 <span>체계적 자기객관화</span>
@@ -158,7 +192,7 @@ const StartHost = () => {
                 <span>간단한 5분 테스트</span>
               </div>
               <div className="shBoxImg">
-                <img src={shImg4} alt="boxImg"/>
+                <img src={shImg4} alt="boxImg" />
               </div>
               <div className="shBoxMainTitle">
                 <span>무료 테스트</span>
@@ -166,35 +200,76 @@ const StartHost = () => {
             </div>
           </div>
           <div className="shUserCount">
-            <span className="shButtonSpanS" style={{
-              fontWeight: "700",
-              backgroundImage: "linear-gradient(90deg, #CFC8D2 0%, #9C76AC " + gradientValue + "%, #CFC8D2 100%)",
-              backgroundClip: "text",
-              WebkitBackgroundClip: "text",
-              color: "transparent"}}>지금까지 {totalNum.toLocaleString()} 명이 참여했어요!</span>
+            <span
+              className="shButtonSpanS"
+              style={{
+                fontWeight: '700',
+                backgroundImage:
+                  'linear-gradient(90deg, #CFC8D2 0%, #9C76AC ' +
+                  gradientValue +
+                  '%, #CFC8D2 100%)',
+                backgroundClip: 'text',
+                WebkitBackgroundClip: 'text',
+                color: 'transparent',
+              }}
+            >
+              지금까지 {totalNum.toLocaleString()} 명이 참여했어요!
+            </span>
           </div>
           <div className="shButtonContainer">
-            <Button onClick={handleStart}className="shButtonL" color="#9C76AC" width={19.7} height={3.4}>
-              <span className="shButtonSpanL" style={{paddingTop: '0.2rem'}}>시작하기</span>
+            <Button
+              onClick={handleStart}
+              className="shButtonL"
+              color={isLoading ? '#D3D3D3' : '#9C76AC'}
+              width={19.7}
+              height={3.4}
+              disabled={isLoading}
+            >
+              <span className="shButtonSpanL" style={{ paddingTop: '0.2rem' }}>
+                {isLoading ? '로딩 중...' : '시작하기'}
+              </span>
             </Button>
-            <Button onClick={handleResult} className="shButtonL" color={canSeeResult ? "#A192C5" : "#F3F3F3"} width={19.7} height={3.4}>
+            <Button
+              onClick={handleResult}
+              className="shButtonL"
+              color={canSeeResult ? '#A192C5' : '#F3F3F3'}
+              width={19.7}
+              height={3.4}
+            >
               <span className="shButtonSpanL">결과 확인하기</span>
             </Button>
             <div className="shShareButtonWrapper">
-              <Button onClick={() => ShareNavigator('SHIFT', 'MZ 자기객관화 테스트', 'https://shift2me.com')} className="testButton" color="#F5F5F5" width={9.2} height={2}>
+              <Button
+                onClick={() =>
+                  ShareNavigator(
+                    'SHIFT',
+                    'MZ 자기객관화 테스트',
+                    'https://shift2me.com'
+                  )
+                }
+                className="testButton"
+                color="#F5F5F5"
+                width={9.2}
+                height={2}
+              >
                 <img src={shareImg} alt="share img"></img>
                 <span>테스트 공유하기</span>
               </Button>
-              <Button onClick={handleShareTest} className="testButton" color="#F5F5F5" width={9.2} height={2}>
+              <Button
+                onClick={handleShareTest}
+                className="testButton"
+                color="#F5F5F5"
+                width={9.2}
+                height={2}
+              >
                 <img src={shareImg} alt="share img"></img>
                 <span>설문 링크 공유하기</span>
               </Button>
             </div>
           </div>
-          <div className="shFooter">
-          </div>
+          <div className="shFooter"></div>
         </div>
-        <MainFooter/>
+        <MainFooter />
       </div>
     </>
   );
